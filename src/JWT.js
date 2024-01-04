947b507cb96e51d9741f1dff3900f99923f16d75
noyke <noy.keren22@gmail.com>
2024-01-04 11:15:13 +0200

    access-token functionality

---
+++ src/JWT.js
@@ -0,0 +1,31 @@
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+dotenv.config();
+
+export const createTokens = (user) => {
+  const accessToken = jwt.sign(
+    { username: user.user_name },
+    process.env.JWT_SECRET
+  );
+
+  return accessToken;
+};
+
+export const validateToken = (req, res, next) => {
+  const authorizationHeader = req.headers["authorization"];
+  const accessToken = authorizationHeader.split(" ")[1];
+
+  if (accessToken === "null") {
+    return res.send({ UserError: "User not Authenticated!" });
+  }
+
+  try {
+    const validToken = jwt.verify(accessToken, process.env.JWT_SECRET);
+    if (validToken) {
+      req.authenticated = true;
+      return next();
+    }
+  } catch (err) {
+    return res.status(400).send({ error: err });
+  }
+};
--- src/index.js
+++ src/index.js
@@ -1,15 +1,25 @@
 import express from "express";
 import bcrypt from "bcrypt";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 import { getUser, createUser } from "./database.js";
+import { createTokens, validateToken } from "./JWT.js";
+
+const corsOptions = {
+  origin: "http://localhost:5173",
+  methods: ["GET", "POST"],
+};
 
 const app = express();
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(cookieParser());
 
 app.post("/register", (req, res) => {
   const { user_name, user_password } = req.body;
+  const user = getUser(user_name);
+  if (user) res.send({ UserError: "User taken!" });
   bcrypt
     .hash(user_password, 10)
     .then((hash) => {
@@ -20,7 +30,7 @@ app.post("/register", (req, res) => {
     })
     .catch((err) => {
       if (err) {
-        res.status(400).send({ error: err });
+        res.send({ error: err });
       }
     });
 });
@@ -30,18 +40,23 @@ app.post("/login", async (req, res) => {
 
   const user = await getUser(user_name);
 
-  if (!user) res.status(400).send({ error: "User doesn't exist!" });
+  if (!user) res.send({ UserError: "User doesn't exist!" });
 
   const dbPassword = user.user_password;
 
   bcrypt.compare(user_password, dbPassword).then((match) => {
     if (!match) {
-      console.log("not match");
-      res.status(400).send({ error: "Wrong password!" });
+      res.send({ UserError: "Wrong password!" });
     } else {
-      res.send("Logged in");
+      const accessToken = createTokens(user);
+
+      res.send(accessToken);
     }
   });
+});
+
+app.get("/todos", validateToken, (req, res) => {
+  res.send("User Authenticated");
 });
 
 app.listen(3000, () => {
